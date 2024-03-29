1740d95b8fa0108ada6d51481e47519c074d7ca1
salema <114165491+vsalema@users.noreply.github.com>
2024-03-29 23:13:10 +0100

    Add files via upload

---
+++ js/tv.js
@@ -0,0 +1,272 @@
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn1'),
+videoHtml = '<iframe src="https://vsalema.github.io/tvi/" width="880" height="700" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd'),
+videoHtml = '<iframe src="https://vsalema.github.io/tvi/" width="880" height="700" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.hasClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn3'),
+videoHtml3 = '<iframe src="https://vsalema.github.io/tvi-africa/" width="880" height="700" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml3);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn2'),
+videoHtml2 = '<iframe src="https://vsalema.github.io/tvi-ficcao/" width="880" height="700" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml2);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn4'),
+videoHtml4 = '<iframe src="https://vsalema.github.io/tvi-reality/" width="880" height="700" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml4);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn5'),
+videoHtml5 = '<iframe src="https://vsalema.github.io/sic/" width="880" height="700" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml5);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn6'),
+videoHtml6 = '<iframe src="https://vsalema.github.io/SICN/" width="880" height="700" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml6);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn7'),
+videoHtml7 = '<iframe src="https://vsalema.github.io/RTP1/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml7);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn8'),
+videoHtml8 = '<iframe src="https://vsalema.github.io/cmtv/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml8);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn9'),
+videoHtml9 = '<iframe src="https://vsalema.github.io/CNN/" width="880" height="700" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml9);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn10'),
+videoHtml10 = '<iframe src="https://vsalema.github.io/SPT2/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml10);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn11'),
+videoHtml11 = '<iframe src="https://vsalema.github.io/ELEVEN2/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml11);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn12'),
+videoHtml12 = '<iframe src="https://vsalema.github.io/BTV/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml12);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn13'),
+videoHtml13 = '<iframe src="https://vsalema.github.io/SPORTING/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml13);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn14'),
+videoHtml14 = '<iframe src="https://vsalema.github.io/11/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml14);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn15'),
+videoHtml15 = '<iframe src="https://vsalema.github.io/NOS-STUDIOS/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml15);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn16'),
+videoHtml16 = '<iframe src="https://vsalema.github.io/Odisseia/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml16);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
+
+var $tv = $('.tv'),
+$screen = $('.screen-content'),
+$btn = $('.js-shtd-btn17'),
+videoHtml17 = '<iframe src="https://vsalema.github.io/NationalGeographic/" width="880" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="no"></iframe>';
+
+$btn.on('click', function () {
+  if ($tv.addClass('_off')) {
+    $tv.removeClass('_off');
+    $screen.html(videoHtml17);
+  } else {
+    $tv.addClass('_off');
+    $screen.empty();
+  }
+});
